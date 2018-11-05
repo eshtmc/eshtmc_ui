522b36ce19b194015f82f09ca28e17e1f4e4f214
eshtmc <github@eshtmc.com>
2018-11-05 23:04:18 +0800

    test for deploy

--- eshtmc_ui/settings.py
+++ eshtmc_ui/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')a^kx0sl*5ooo&-q4f-*itew0t_d-mm9lw4o*qdp!(cnro3k(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "127.0.0.1:8000"
+bind = "0.0.0.0:80"
 workers = 2
 # workers是工作线程数，一般设置成：服务器CPU个数 + 1
 # errorlog = '/eshtmc_ui/gunicorn.error.log'
